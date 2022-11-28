d4f7799ed8e91b9385e2e888e2d86f34fa9db92b
Kent Ou <84220825+kentnsw@users.noreply.github.com>
2022-11-28 14:56:47 +1100

    c7n - cli - refactor enable vars option

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -538,18 +538,20 @@ class RunTest(CliTest):
         # NOTE case 1 no vars file specified    
         self.run_and_expect_success(cmd)
         assert not os.path.exists(resources_file)
+
         # NOTE case 2 vars file specified, not meet conditions
         vars_file = self.write_policy_file(
             {"vars": {"holiday_start": "2020/01/01"}}
         )
+        cmd = ["custodian", "run", "-s", temp_dir, "--vars", vars_file, policy_file]
         self.run_and_expect_success(cmd)
         assert not os.path.exists(resources_file)
+
         # NOTE case 3 vars file specified, meet conditions
         vars_file = self.write_policy_file(
             {"vars": {"holiday_start": "2099/01/01"}}
         )
-        cmd.pop()
-        cmd.extend(["--vars", vars_file, policy_file])
+        cmd = ["custodian", "run", "-s", temp_dir, "--vars", vars_file, policy_file]
         self.run_and_expect_success(cmd)
         assert os.path.exists(resources_file)
 
